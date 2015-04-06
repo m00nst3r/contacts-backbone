f22cb3b45fddbdc4fea598b27ad3ce6076333621
Andrew <akolisnyk@malkosua.com>
2015-04-06 17:32:27 +0300

    added tests for matching function

--- js/app.js
+++ js/app.js
@@ -1,3 +1,3 @@
-function hello(){
-    return "hello, world";
+function matches(v){
+    return /^box.*|^po.*box.*|^p\.o\..*box.*/i.test(v) === false;
 }
--- test/app-test.js
+++ test/app-test.js
@@ -1,7 +1,17 @@
-describe('JS: indexController: ', function(){
-    describe("hello", function () {
-        it('should return text', function () {
-            expect(hello()).toBe('hello');
-        });
+describe("matches", function () {
+    it('should return false on regular expression with text "box" ', function () {
+        expect(matches('box')).toBe(false);
+    });
+    it('should return false on regular expression with text "pobox" ', function () {
+        expect(matches('pobox')).toBe(false);
+    });
+    it('should return false on regular expression with text "p.o.box" ', function () {
+        expect(matches('p.o.box')).toBe(false);
+    });
+    it('should return false on regular expression with text "p.o.fooboxbar" ', function () {
+        expect(matches('p.o.fooboxbar')).toBe(false);
+    });
+    it('should return true on regular expression with text "1box" ', function () {
+        expect(matches('1box')).toBe(true);
     });
 });
