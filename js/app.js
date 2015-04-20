0e753180bcf21e8a2f39fb84cc729cae5543fc50
Andrew <akolisnyk@malkosua.com>
2015-04-21 00:16:45 +0300

    small code refactoring

--- js/app.js
+++ js/app.js
@@ -2,13 +2,9 @@ function matches(v){
     return /^box.*|^po.*box.*|^p\.o\..*box.*/i.test(v);
 }
 function invertOffset(offset){
-    var str;
     if( /^-/i.test(offset) ){
-        str = offset.toString().slice(1, offset.length);
-        return parseInt(str);
+        return parseInt(offset.toString().slice(1, offset.length));
     }else{
-        str = offset.toString();
-        str = '-' + str;
-        return parseInt(str);
+        return parseInt('-' + offset.toString());
     }
 }
